10feceba2a170f06e903007185c3eeaf54b20890
Aboubacar Tuo <aboubacar.tuo@cea.fr>
2022-10-03 21:05:10 +0200

    update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221003.203807';
+const cacheName = 'chirpy-20221003.210400';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
