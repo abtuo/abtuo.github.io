8e991ab723dbd10c398388b6214abee775fdb565
Aboubacar Tuo <aboubacar.tuo@cea.fr>
2022-11-25 12:33:30 +0100

    new post

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -19,6 +19,8 @@ const resource = [
   
     '/about/',
   
+    '/posts/',
+  
 
   /* --- Favicons & compressed JS --- */
   
--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20221118.203954';
+const cacheName = 'chirpy-20221125.122832';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
